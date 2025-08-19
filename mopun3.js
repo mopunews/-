0a824e380e41545ed38abed989d4f2ca49e6114e
mopunews <ppoonnyyoonngg@gmail.com>
2025-08-20 01:37:52 +0900

    Update mopun3.js

--- mopun3.js
+++ mopun3.js
@@ -1,7 +1,7 @@
 const newsList = [
   {
-    title: "기후 변화가 심각해지고 있어요",
-    summary: "2025년, 이상기후 현상이 전 세계적으로 늘어나고 있습니다.",
+    title: "모푸의 컴퓨터 문제가 모푸(관리자)의 문제가 아닐 수도 있다는 사실이 드러나... 모푸를 질책했던 푸리들은 미안한 마음에 공겜 10개 면제권 준다는...",
+    summary: "사랑해요",
     date: "2025-08-20"
   }
 ];
@@ -12,4 +12,5 @@ newsList.forEach(news => {
   const div = document.createElement("div");
   div.innerHTML = `<h2>${news.title}</h2><p>${news.summary}</p><small>${news.date}</small>`;
   container.appendChild(div);
+
 });
